8521049f5b19114d099ba8470cf9842ba8eb10cf
rwathena <rwathena3600@gmail.com>
2015-05-31 10:21:29 +0800

    Make the class name be same for socket communication

--- src/com/zeroapp/parking/view/SigninFragment.java
+++ src/com/zeroapp/parking/view/SigninFragment.java
@@ -23,7 +23,6 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.zeroapp.parking.R;
-import com.zeroapp.parking.message.AMessage;
 import com.zeroapp.parking.message.ClientServerMessage;
 import com.zeroapp.parking.message.MessageConst;
 import com.zeroapp.utils.Log;
@@ -41,49 +40,50 @@ import com.zeroapp.utils.Log;
  */
 public class SigninFragment extends BaseFragment {
 
-    private View mainView;
-    private EditText editTextAccount;
-    private EditText editTextPwd;
-    private Button buttonSingin;
-    private Button buttonSingup;
-    private MainActivity mainActivity;
+	private View mainView;
+	private EditText editTextAccount;
+	private EditText editTextPwd;
+	private Button buttonSingin;
+	private Button buttonSingup;
+	private MainActivity mainActivity;
 
-    @Override
-    public void onAttach(Activity activity) {
-        Log.i("onAttach");
-        super.onAttach(activity);
-        mainActivity = (MainActivity) getActivity();
-//        reqData = new HashMap<String, Object>();
-    }
+	@Override
+	public void onAttach(Activity activity) {
+		Log.i("onAttach");
+		super.onAttach(activity);
+		mainActivity = (MainActivity) getActivity();
+		// reqData = new HashMap<String, Object>();
+	}
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        Log.i("onCreateView");
-        mainView = inflater.inflate(R.layout.fragment_signin, null);
-        editTextAccount = (EditText) mainView.findViewById(R.id.et_account);
-        editTextPwd = (EditText) mainView.findViewById(R.id.et_password);
-        buttonSingin = (Button) mainView.findViewById(R.id.btn_signin);
-        buttonSingup = (Button) mainView.findViewById(R.id.btn_signup);
-        buttonSingin.setOnClickListener(new OnClickListener() {
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		Log.i("onCreateView");
+		mainView = inflater.inflate(R.layout.fragment_signin, null);
+		editTextAccount = (EditText) mainView.findViewById(R.id.et_account);
+		editTextPwd = (EditText) mainView.findViewById(R.id.et_password);
+		buttonSingin = (Button) mainView.findViewById(R.id.btn_signin);
+		buttonSingup = (Button) mainView.findViewById(R.id.btn_signup);
+		buttonSingin.setOnClickListener(new OnClickListener() {
 
-            @Override
-            public void onClick(View v) {
-                AMessage m = new ClientServerMessage();
-                m.setMessageType(MessageConst.MessageType.MSG_TYPE_USER_SIGN_IN);
-                m.setMessageContent("test");
-                mainActivity.mClient.sendMessageToServer(m);
+			@Override
+			public void onClick(View v) {
+				ClientServerMessage m = new ClientServerMessage();
+				m.setMessageType(MessageConst.MessageType.MSG_TYPE_USER_SIGN_IN);
+				m.setMessageContent("test");
+				mainActivity.mClient.sendMessageToServer(m);
 
-            }
-        });
-        buttonSingup.setOnClickListener(new OnClickListener() {
+			}
+		});
+		buttonSingup.setOnClickListener(new OnClickListener() {
 
-            @Override
-            public void onClick(View v) {
-                mainActivity.showFragment(v.getId());
+			@Override
+			public void onClick(View v) {
+				mainActivity.showFragment(v.getId());
 
-            }
-        });
-        return mainView;
-    }
+			}
+		});
+		return mainView;
+	}
 
 }
